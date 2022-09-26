3cef966c2c20bfef0c517dbfb1ea949781ff44fd
MalokeiroDigital <javaentra21@yahoo.com>
2022-09-26 10:40:05 -0300

    update class AtendenteEntity

--- src/main/java/com/consultorio/consultoriomedico/model/AtendenteEntity.java
+++ src/main/java/com/consultorio/consultoriomedico/model/AtendenteEntity.java
@@ -1,6 +1,22 @@
 package com.consultorio.consultoriomedico.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="atendente")
 public class AtendenteEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(nullable = false)
     private String nome;
 }
+
+//https://www.devmedia.com.br/jpa-como-usar-a-anotacao-generatedvalue/38592
